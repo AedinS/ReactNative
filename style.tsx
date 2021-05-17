3a793a605476e2e0c188a862160f8f21b200cfdd
AedinS <79467832+AedinS@users.noreply.github.com>
2021-05-17 17:28:02 +0900

    Create style.tsx

---
+++ style.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+
+const LotsOfStyles = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.red}>just red</Text>
+      <Text style={styles.bigBlue}>bigBlue</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    </View>
+  )
+}
+// body
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 30,
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+// Using StyleSheet -> styles
+
+export default LotsOfStyles;
